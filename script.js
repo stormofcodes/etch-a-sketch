92e2c050caa3330335030246d2d03326d73d7c21
stormofcodes <stormofcodes@gmail.com>
2025-04-26 19:26:33 +0200

    Add eventlistener for erasebutton and conditional statements to replace background color in grid divs with empty strings

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
 const content = document.querySelector('.content');
 const slider = document.querySelector('.slider');
+const colorInput = document.querySelector('.colorInput');
+const eraseButton = document.querySelector('.eraseButton');
 
 let isFilling = false;
+let isErasing = false;
+let currentColor = colorInput.value;
+
+colorInput.addEventListener('input', function() {
+currentColor = colorInput.value;
+});
 
 function createGrid(size) {
     while (content.firstChild) {
@@ -15,21 +23,34 @@ for (let i = 0; i < size * size; i++) {
     square.classList.add('square');
     square.style.width = squareSize + '%';
     square.style.height = squareSize + '%';
-    content.appendChild(square);
+    content.appendChild(square); 
 
     square.addEventListener('mouseover', function () {
         if (isFilling) {
-            this.classList.add('filled');
+            this.style.backgroundColor = currentColor;
+        } else if (isErasing) {
+            this.style.backgroundColor = '';
         }
     });
     square.addEventListener('mousedown', function() {
         isFilling = !isFilling;
         if (isFilling) {
-            this.classList.add('filled');
+            this.style.backgroundColor = currentColor;
+        } else if (isErasing) {
+            this.style.backgroundColor = '';
         }
     });
     }
 }
+
+eraseButton.addEventListener('click', function () {
+    isErasing = !isErasing;
+    if (isErasing) {
+        eraseButton.style.backgroundColor = 'lightgray';
+    } else {
+        eraseButton.style.backgroundColor = '';
+    }
+});
 
 slider.addEventListener('input', function() {
     const size = slider.value;
